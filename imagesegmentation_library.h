a9a95686823967cdd04b8e34d154ff42aecbcf7b
Yorck Ramachers <y.a.ramachers@warwick.ac.uk>
2018-04-03 18:48:03 +0100

    included graphclustering headers

--- imagesegmentation_library.h
+++ imagesegmentation_library.h
@@ -4,6 +4,8 @@
 // std libraries
 #include <unordered_map>
 #include <list>
+#include <set>
+#include <unordered_set>
 #include <vector>
 #include <utility>
 
@@ -63,6 +65,42 @@ public:
 };
 
 
+// Usage as helper class: Create complex Graph nodes or edges
+// in a countable container independent of this graph class. Once finished
+// with a static container content of those complex vertex objects,
+// all it takes is to use the unique element index in that container
+// to initate this graph and use it entirely with integers throughout.
+// Any result from here can then address complex objects in their 
+// container by index, again outside this class. 
+class Graph
+{
+  // undirected, unweighted graph object using integers as Node identifiers
+  // for simple usage as a pure helper class.
+private:
+  int V; // number of vertices
+  int INT_MAX;
+  
+  std::vector<std::unordered_set<int>> adj; // adjacency lists
+  
+  std::vector<std::vector<int>> allPaths; // result path
+  std::vector<int> currentPath; // helper path
+  
+  void dfs(std::vector<std::unordered_set<int>>& prev,
+	   int node); // depth-first search helper function
+  
+public:
+  
+  Graph(int nv); // Constructor with number of vertices
+  
+  void addEdge(int v, int w); // function to add an edge to graph
+  bool isReachable(int s, int d); // returns true if there is a path from s to d
+  std::vector<std::vector<int>> bfsPaths(int start, int target); // all paths between s and t
+  bool singleNode(int node); // is a single node (one edge only) if true
+  std::unordered_set<int> nodes(); // return node container as set
+};
+
+
+
 // Used for the more complex clustering challenges
 class GraphClusterer {
 
@@ -73,7 +111,23 @@ private:
   int height;
   std::unordered_map<unsigned int, std::list<Pixel> > cls;
   
+  // index store of nodes with column map index where nodes hold pixel indices
+  std::unordered_map<int, std::vector<std::vector<int> > > store;
+
+  std::set<std::pair<int, int> > vertices; // unique nodes set
+  std::vector<std::pair<int, int> > nodes; // countable container for indexing
+  std::vector<std::pair<Pixel, Pixel> > edges; // node connections
+
+  // functions
+  void make_edges();
+  bool nodes_connected(std::vector<int> va, std::vector<int> vb);
+
 protected:
+  void cluster_withgraph(); // uses store and cls
+  void translate(std::list<std::vector<std::vector<int> > > temp);
+  std::vector<int> all_deadends(Graph gg);
+  std::vector<int> column_nodes(Graph gg, int col);
+  std::vector<std::vector<int> > oneDcluster(std::vector<int> data);
 
 
 public:
@@ -83,10 +137,16 @@ public:
     height=h;
   }
   ~GraphClusterer() {
+    cls.clear();
+    store.clear();
+    nodes.clear();
+    vertices.clear();
+    edges.clear();
   }
 
   void cluster(std::vector<bool> data); // input for GraphClusterer
   std::unordered_map<unsigned int, std::list<Pixel> > getClusters();
+  std::vector<std::pair<int, int> > getNodes() {return nodes;}
 };
 
 
