6df57eecb1abd4772578926ec157b35d636e968a
Yorck Ramachers <y.a.ramachers@warwick.ac.uk>
2018-05-22 16:58:08 +0100

    SimpleFit object with helpers implemented and a fit trial test.

--- imagesegmentation_library.h
+++ imagesegmentation_library.h
@@ -10,6 +10,11 @@
 #include <valarray>
 #include <utility>
 
+// ROOT
+#include <TGraph2DErrors.h>
+#include <Fit/Fitter.h>
+#include <Fit/FitResult.h>
+
 #define DUMMY 11111
 
 // MetaInfo translates uniquely to Geiger Hit
@@ -225,6 +230,201 @@ public:
 
   // output, cleaned cluster collection
   std::unordered_map<unsigned int, std::vector<MetaInfo> > getClusters() {return clustercopy;}
+
+};
+
+
+
+//****************************
+// modify cluster map 
+// SimpleFit acts as filter
+// to select clusters according
+// to geometry models, here
+// a line in 3D and a helix.
+// If these models are not 
+// fit cleanly then the cluster
+// can be deselected.
+// Uses tracker data only.
+//****************************
+// store the more complex fit result data 
+class FitLine {
+ private:
+  const ROOT::Fit::FitResult* res;
+  // store internally
+  double c2;
+  double p;
+  double xyi;
+  double xys;
+  double xzi;
+  double xzs;
+  double err0;
+  double err1;
+  double err2;
+  double err3;
+  int covst;
+
+ public: 
+  FitLine() {res=0;} // default
+  FitLine(const ROOT::Fit::FitResult& fr) {
+    res = &fr;
+    c2  = res->Chi2(); ;
+    p   = res->Prob();
+    xyi = res->Parameter(0);
+    xys = res->Parameter(1);
+    xzi = res->Parameter(2);
+    xzs = res->Parameter(3);
+    err0= res->ParError(0);
+    err1= res->ParError(1);
+    err2= res->ParError(2);
+    err3= res->ParError(3);
+    covst= res->CovMatrixStatus();//=0 not calculated, =1 approximated, =2 made pos def , =3 accurate
+  }
+  ~FitLine() {
+  }
+  
+  double chi2() {return c2;}
+  double prob() {return p;}
+  
+  double xyintercept() {return xyi;}
+  double xyslope() {return xys;}
+  double xzintercept() {return xzi;}
+  double xzslope() {return xzs;}
+  double xyierr() {return err0;}
+  double xyserr() {return err1;}
+  double xzierr() {return err2;}
+  double xzserr() {return err3;}
+  
+  int covstatus() {return covst;}
+};
+
+
+class FitHelix {
+ private:
+  const ROOT::Fit::FitResult* res;
+  double c2;
+  double p;
+  double xr;
+  double yr;
+  double zor;
+  double om;
+  double tanl;
+  double bf;
+  double err0;
+  double err1;
+  double err2;
+  double err3;
+  double err4;
+  int covst;
+
+ public:
+  FitHelix() {res=0;} // default
+  FitHelix(const ROOT::Fit::FitResult& fr) {
+    res = &fr;
+  // store internally
+    c2  = res->Chi2(); ;
+    p   = res->Prob();
+    xr  = res->Parameter(0);
+    yr  = res->Parameter(1);
+    zor = res->Parameter(2);
+    om  = res->Parameter(3);
+    tanl= res->Parameter(4);
+    bf  = res->Parameter(5);
+    err0 = res->ParError(0);
+    err1 = res->ParError(1);
+    err2 = res->ParError(2);
+    err3 = res->ParError(3);
+    err4 = res->ParError(4);
+    covst= res->CovMatrixStatus();//=0 not calculated, =1 approximated, =2 made pos def , =3 accurate
+  }
+  ~FitHelix() {
+  }
+
+  double chi2() {return c2;}
+  double prob() {return p;}
+  
+  double xref() {return xr;}
+  double yref() {return yr;}
+  double z0() {return zor;}
+  double omega() {return om;}
+  double tanlambda() {return tanl;}
+  double bfield() {return bf;}
+  double xreferr() {return err0;}
+  double yreferr() {return err1;}
+  double z0err() {return err2;}
+  double omegaerr() {return err3;}
+  double tanlerr() {return err4;}
+  
+  int covstatus() {return covst;}
+};
+
+
+//**********************
+// modify cluster map by
+// removing clusters not 
+// passing fit criteria.
+//**********************
+class SimpleFit {
+
+private:
+  double threshold; // pass for acceptable fit
+  double bfield;    // fixed fit parameter
+  double invradius; // critical fit parameter start value
+  // fixed errors for fitting
+  double errxy; // geiger cell error in indices
+  double errz; // z error [mm]
+  
+  std::vector<unsigned int> accept; // cluster id's accepted
+  std::unordered_map<unsigned int, std::vector<MetaInfo> > clusters;
+
+  // ROOT data container
+  TGraph2DErrors* tgraph;
+  ROOT::Fit::Fitter fitter;
+  FitLine LineResult;
+  FitHelix HelixResult;
+  std::vector<std::pair<unsigned int, FitLine> > goodLineFits;
+  std::vector<std::pair<unsigned int, FitHelix> > goodHelixFits;
+
+
+protected:
+  bool fitline(unsigned int clsid);
+  bool fithelix(unsigned int clsid);
+
+
+public:
+
+  SimpleFit() {
+    threshold = 0.01; // test chi2 probability value
+    bfield = 0.0;
+    invradius = 0.01;
+    errxy = 0.5; // index value error
+    errz = 9.0;  // genuine z-error
+    tgraph = new TGraph2DErrors(); // construct only once, clear thereafter
+  } // default constructor
+  
+  ~SimpleFit() {
+    accept.clear();
+    clusters.clear();
+    goodLineFits.clear();
+    goodHelixFits.clear();
+    if (tgraph)
+      delete tgraph;
+  }
+
+  // init and setting functions
+  void init(std::unordered_map<unsigned int, std::vector<MetaInfo> >& cls); // full info clusters
+  void setThreshold(double t) {threshold = t;} // probability threshold
+  void setBfield(double t) {bfield = t;}
+  void setOmega(double t) {invradius = t;}
+  void setErrorXY(double t) {errxy = t;}
+  void setErrorZ(double t) {errz = t;}
+
+  // action functions
+  void selectClusters(); // remove clusters not passing fitting criteria
+
+  // output, cleaned cluster collection
+  std::unordered_map<unsigned int, std::vector<MetaInfo> > getClusters();
+  std::vector<std::pair<unsigned int, FitLine> > getLineFits() {return goodLineFits;}
+  std::vector<std::pair<unsigned int, FitHelix> > getHelixFits() {return goodHelixFits;}
 
 };
 
